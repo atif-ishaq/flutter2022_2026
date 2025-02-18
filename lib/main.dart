0cf0f9fd47be09d79e67e25990dd177347cac7f1
Atif Ishaq <gemeni4gemeni@gmail.com>
2025-02-18 10:26:07 +0500

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
 
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      title: "App Title",
+    home:Scaffold(
+      appBar: AppBar(
+        title: Text("App Bar Title"),
+        ),
+   body: Center(
+    child:Text("Fluter Body") ,
+   ),
+    ),
+    );
+    
+     }
+
+
+
+
+}
+/*
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -12,3 +40,4 @@ void main(){
     ),
   ),);
 }
+*/
